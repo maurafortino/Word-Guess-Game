5a0acccfadcf913b86f659f98eb6d0f6c3ae292e
maurafortino <maurafortino@gmail.com>
2019-08-22 22:13:12 -0400

    added images to the images folder and edited js file so blanks and a photo appear on start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,43 +2,45 @@
 var characterList =[
 
     characterOne = {
-        name: 'jo march',
-        spaces: '_ _',
-        bookTitle:'Little Women',
+        name: 'celie johnson',
+        bookTitle: 'The Color Purple',
+        img: "assets/images/celie-johnson.jpeg"
+
 },
     characterTwo = {
         name: 'starr carter',
-        spaces: '_ _ _ _ _',
         bookTitle: 'The Hate U Give',
+        img: "assets/images/starr-carter.jpeg"
 
 },
     characterThree = {
-        name: 'bri jackson',
-        firstNameSpaces: '_ _ _',
-        lastNameSpaces:'_ _ _ _ _ _ _',
-        bookTitle: 'On The Come Up',
+        name: 'minerva mirabal',
+        bookTitle: 'In the Time of the Butterflies',
+        img: "assets/images/minerva-mirabal.jpg"
 },
 
     characterFour = {
-        name: 'hermionie',
-        spaces: '_ _ _ _ _ _ _ _ _',
-        bookTitle: 'Harry Potter'
+        name: 'hermionie granger',
+        bookTitle: 'Harry Potter',
+        img: "assets/images/hermione-granger.jpeg"
 },
 
     characterFive = { 
-        name: 'zelie',
-        spaces: '_ _ _ _ _',
-        bookTitle: 'Children of Blood and Bone'
+        name: 'rachel chu',
+        bookTitle: 'Crazy Rich Asians',
+        img: "assets/images/rachel-chu.png"
+        
 }
 ];
 
 var userGuess = document.getElementById("guess");
 var targetAnswer = document.getElementById("answer");
-var tagertGuessed = document.getElementById("guessedLetters");
+var tagertGuessed = document.getElementById("guessed-letters");
+var targetPhoto = document.getElementById("image-place-holder");
 var randomCharacter = characterList[Math.floor(Math.random() *5)];
 var characterName = randomCharacter.name;
-var characterSpaces = randomCharacter.firstNamespaces + " " + randoncomCharacter.lastNameSpaces;
 var characterBook = randomCharacter.bookTitle;
+var characterImage = randomCharacter.img;
 var newSpan = document.createElement("span");
 
 
@@ -46,22 +48,38 @@ var newSpan = document.createElement("span");
 
 //this function starts the game 
 function startGame(){
-    targetAnswer.textContent = characterSpaces;
-    console.log(characterName)
+    for(var i = 0; i < characterName.length; i++){
+
+        //adds underscores to the answer div based on the chosen random character's name
+        var underscore = document.createElement("div");
+        underscore.innerHTML = "_ "
+        underscore.classList.add("add-underscore");
+        underscore.style.display = "inline";
+        underscore.setAttribute("data-letter", characterName[i]);
+        targetAnswer.append(underscore);
+    }
+        //adds an image to the imagePlaceHolder div based on the random charater's name
+
+    var image = document.getElementById("image-place-holder");
+    image.setAttribute("src", characterImage);
+     
     
 };
 
 //if user guesses the answer right, function will display the answer
-function moveLetter(){
-    if(randomCharacter.indexOf(userGuess) > -1 && randomCharacter.indexOf(userGuess) < randomCharacter.length){
-        userGuess.addEventListener("keydown", function(event){
-        newSpan.textContent = event.key;
-        document.getElementById("random").appendChild(newSpan);
-        });
-    } else{
-        alert("wrong!");
-    };
-};
+// function moveLetter(){
+//     if(randomCharacter.indexOf(userGuess) > -1 && randomCharacter.indexOf(userGuess) < randomCharacter.length){
+//         userGuess.addEventListener("keydown", function(event){
+//         newSpan.textContent = event.key;
+//         document.getElementById("random").appendChild(newSpan);
+//         });
+//     } else{
+//         alert("wrong!");
+//     };
+// };
+
+//a for loop to generate underscores
+
 
 
 
