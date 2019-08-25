ede50d48a7125db01947776dfac00911c7e91f6e
maurafortino <maurafortino@gmail.com>
2019-08-24 23:38:36 -0400

    whole game minus guessed letters displaying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,114 +1,147 @@
 //this function shows the game when a person clicks the 'Let's play button'
-var characterList =[
+var characterList = [
 
     characterOne = {
-        name: 'celie johnson',
-        bookTitle: 'The Color Purple',
-        img: "assets/images/celie-johnson.jpeg"
-
-},
-    characterTwo = {
-        name: 'starr carter',
-        bookTitle: 'The Hate U Give',
-        img: "assets/images/starr-carter.jpeg"
-
-},
-    characterThree = {
-        name: 'minerva mirabal',
-        bookTitle: 'In the Time of the Butterflies',
+        name: 'minerva',
+        spelling: ['m', 'i', 'n', 'e', 'r', 'v', 'a'],
         img: "assets/images/minerva-mirabal.jpg"
-},
 
-    characterFour = {
-        name: 'hermionie granger',
-        bookTitle: 'Harry Potter',
-        img: "assets/images/hermione-granger.jpeg"
-},
+    },
+    characterTwo = {
+        name: 'melba',
+        spelling: ['m', 'e', 'l', 'b', 'a'],
+        img: "assets/images/melba.jpeg"
+    },
+        characterThree = {
+            name: 'lisbeth',
+            spelling: ['l', 'i', 's', 'b', 'e', 't', 'h'],
+            img: "assets/images/lisbeth-salander.png"
+    },
 
-    characterFive = { 
-        name: 'rachel chu',
-        bookTitle: 'Crazy Rich Asians',
-        img: "assets/images/rachel-chu.png"
-        
-}
+        characterFour = {
+            name: 'scout',
+            spelling: ['s', 'c', 'o', 'u', 't'],
+            img: "assets/images/scout-finch.jpeg"
+    },
+
+        characterFive = { 
+            name: 'rachel',
+            spelling: ['r', 'a', 'c', 'h', 'e', 'l'],
+            img: "assets/images/rachel-chu.png"
+
+     }
 ];
-var leftColumn = document.getElementById("left-column");
-var rightColumn = document.getElementById("right-column");
-var userGuess = document.getElementById("guess");
-var targetAnswer = document.getElementById("answer");
-var targetGuessed = document.getElementById("guessed-letters");
-var targetPhoto = document.getElementById("image-place-holder");
-var randomCharacter = characterList[Math.floor(Math.random() *5)];
+var randomCharacter = characterList[Math.floor(Math.random() * characterList.length)];
 var characterName = randomCharacter.name;
 var characterBook = randomCharacter.bookTitle;
 var characterImage = randomCharacter.img;
-var newSpan = document.createElement("span");
+var characterSpelling = randomCharacter.spelling;
+var targetWinsText = document.getElementById("wins");
+var targetLossesText = document.getElementById("losses");
+var targetAnswerText = document.getElementById("answer");
+var targetGuessedLettersText = document.getElementById("guessed-letters");
+var targetPhotoSpace = document.getElementById("image-place-holder");
+var targetGuessesText = document.getElementById("guesses-left");
+//adds styles to the columns
+var leftColumn = document.getElementById("left-column");
+var rightColumn = document.getElementById("right-column");
+leftColumn.classList.add("game-style");
+rightColumn.classList.add("game-style");
+
+var answerArray = [];
+var lettersGuessed = []; 
+var guessesLeft = 10;
+
+function updateScore() {
+    targetWinsText.textContent = "wins: " + wins++;
+}
 
 var wins = 0;
-var targetWins = document.querySelectorAll('#wins')[0];
-targetWins.textContent = wins;
-targetWins.classList.add("score-style");
-
+targetWinsText.textContent = "wins: " + wins;
 var losses = 0;
-var targetLosses = document.getElementById("losses");
-//targetLosses.textContent = "losses: " + losses;
-targetLosses.classList.add("score-style");
-
-var guesses = 10;
-var targetGuesses = document.getElementById("guesses-left");
-targetGuesses.textContent = "guesses left: " + guesses;
-var lettersGuessed = []; //letters guessed start with an empty array
-targetGuessed.textContent = "letters guessed: " + lettersGuessed;
-
-    // //shows the score which starts at 0 wins and 0 losses
-    // wins = document.getElementById("wins");
-    // wins.textContent = "wins: " + 0;
-    //     //adds style to the wins section
- 
-
-    // losses = document.getElementById("losses");
-    // losses.textContent = "losses: " + 0;
-    //     //adds style to the losses section
-    //     losses.classList.add("score-style");
-
-    // //shows how many guesses you have left and the letters you have guessed
-    // guesses = document.getElementById("guesses-left");
-    // guesses.textContent = "guesses left: " + 10;
+targetLossesText.textContent = "losses: " + losses;
+targetGuessesText.innerHTML = guessesLeft;
+targetGuessedLettersText.textContent = lettersGuessed;
+targetAnswerText.classList.add("answer-style");
+targetWinsText.classList.add("score-style");
+targetLossesText.classList.add("score-style");
 
 
-    //adds styles to the columns
-    leftColumn.classList.add("game-style");
-    rightColumn.classList.add("game-style");
 //this function starts the game 
-function startGame(){
+function startGame() {
 
-//adds an image to the imagePlaceHolder div based on the random charater's name 
-var image = document.getElementById("image-place-holder");
-image.setAttribute("src", characterImage);
+    //adds an image to the imagePlaceHolder div based on the random charater's name 
+    var image = document.getElementById("image-place-holder");
+    image.setAttribute("src", characterImage);
 
     //for loop to go through the characters name 
-    for(var i = 0; i < characterName.length; i++){
+    for (var i = 0; i < characterName.length; i++) {
 
-    //adds underscores to the answer div based on the chosen random character's name
-    var underscore = document.createElement("div");
-    underscore.innerHTML = "_ "
-    underscore.classList.add("add-underscore");
-    underscore.style.display = "inline";
-    underscore.setAttribute("data-letter", characterName[i]);
-    targetAnswer.append(underscore);
+        //adds underscores to the answer div based on the chosen random character's name
+        var underscore = document.createElement("div");
+        underscore.innerHTML = "_ "
+        underscore.classList.add("add-underscore");
+        underscore.style.display = "inline";
+        underscore.setAttribute("data-letter", characterSpelling[i]);
+        targetAnswerText.append(underscore);
     };
-};
-
-//this function shows a hint when hint button is clicked
-function showHint(){
-    var newP = document.createElement("p");
-    document.getElementById("hint").appendChild(newP);
-    newP.innerHTML = "this character appears in " + "<em>" + characterBook + "</em>";
 
 };
 
+function replay() {
+    var playAgain = confirm("do you want to play again?")
+    if (playAgain === true) {
+    answerArray = [];
+    targetAnswerText.textContent = "";
+    guessesLeft = 10;
+    lettersGessed = [];
+    randomCharacter = characterList[Math.floor(Math.random() * characterList.length)];
+    characterName = randomCharacter.name;
+    characterBook = randomCharacter.bookTitle;
+    characterImage = randomCharacter.img;
+    characterSpelling = randomCharacter.spelling;
 
+    var image = document.getElementById("image-place-holder");
+    image.setAttribute("src", characterImage);
 
+    //for loop to go through the characters name 
+    for (var i = 0; i < characterName.length; i++) {
+        //adds underscores to the answer div based on the chosen random character's name
+        var underscore = document.createElement("div");
+        underscore.innerHTML = "_ "
+        underscore.classList.add("add-underscore");
+        underscore.style.display = "inline";
+        underscore.setAttribute("data-letter", characterSpelling[i]);
+        targetAnswerText.appendChild(underscore);
+    };
+}
+}
 
+function replaceUnderscore() {
+    targetAnswerText.textContent = answerArray.join(" ");
+};
 
+document.onkeyup = function letterPressed() {
+    var userGuess = event.key
+    var checkGuess = characterSpelling.includes(userGuess);
+    var checkLetters = lettersGuessed.includes(userGuess);
+    // var lettersChecked = lettersGuessed.includes(userGuess);
 
+    if (checkGuess === true && guessesLeft > 1) {
+
+        var x = characterSpelling.indexOf(userGuess);
+        answerArray[x] = userGuess;
+        replaceUnderscore();
+
+        if (JSON.stringify(characterSpelling) === JSON.stringify(answerArray)) {
+            updateScore();
+            alert("you win this round!");
+            replay();
+            }
+        }else if(checkGuess === false && guessesLeft > 1 && checkLetters === false){
+            targetGuessesText.textContent = guessesLeft--;
+            lettersGuessed.push(userGuess);
+        }else if(checkGuess === false && guessesLeft <= 1){
+            alert('you lose. refresh page to play again'); 
+        }
+    };
